100893d6b474c420483da3c4d7ee94dcf394abb6
Abhishek <abhishek.sawant@simformsolutions.com>
2024-11-07 11:33:16 +0530

    feat: making boilerplate ready

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+let currentTheme = 'light'
+
+window.addEventListener('load', () => {
+  const theme = localStorage.getItem('theme')
+  if (theme === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    currentTheme = 'dark'
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+    currentTheme = 'light'
+  }
+})
+
+function themeSwitcher() {
+  currentTheme = currentTheme === 'light' ? 'dark' : 'light'
+  document.documentElement.setAttribute('data-theme', currentTheme)
+  localStorage.setItem('theme', currentTheme)
+}
